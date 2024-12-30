3e95c8b83c0523fbefc5d162091eaef97d6909f5
Akash Chaurasia <akash.chaurasia3870@gmail.com>
2024-12-30 22:02:12 +0530

    solution day 4 : rotate an array by x step

---
+++ Day4/Main.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+
+        int t = Integer.parseInt(in.readLine().trim());
+        while (t-- > 0) {
+            String line = in.readLine();
+            String[] tokens = line.split(" ");
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int d = Integer.parseInt(in.readLine().trim()); // rotation count (key)
+            int n = array.size();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) arr[i] = array.get(i);
+
+            new Solution().rotateArr(arr, d); // rotating the array
+            StringBuilder sb = new StringBuilder();
+
+            // printing the elements of the array
+            for (int value : arr) sb.append(value).append(" ");
+            out.println(sb.toString().trim());
+
+            out.println("~");
+        }
+        out.flush();
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to rotate an array by d elements in counter-clockwise direction.
+    static void rotateArr(int arr[], int d) {
+        // add your code here
+        d = d % arr.length ;
+        rotateArray(arr,0,d-1);
+        rotateArray(arr,d,arr.length-1);
+        rotateArray(arr,0,arr.length-1);
+        
+        
+    }
+    
+    public static void rotateArray(int arr[] , int start , int end){
+        
+        
+        while(start<end && start!=end)
+        {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+    }
+}
